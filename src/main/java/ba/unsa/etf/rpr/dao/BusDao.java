29fb8ba8e97a83755b754ee6ee2c43410c16ee68
Ernad Zuga <ezuga1@etf.unsa.ba>
2022-12-13 22:30:12 +0100

    BusDao interface created

---
+++ src/main/java/ba/unsa/etf/rpr/dao/BusDao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.dao;
+import ba.unsa.etf.rpr.domain.Bus;
+
+/**
+ * Dao interface for Bus domain bean
+ *
+ * @author Ernad Zuga
+ */
+public interface BusDao extends Dao<Bus>{
+    
+}
