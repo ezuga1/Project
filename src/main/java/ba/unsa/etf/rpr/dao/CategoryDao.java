9309d862a3e094a35b630d6f1956ab90fd8a08b5
Ernad Zuga <ezuga1@etf.unsa.ba>
2022-12-17 20:10:53 +0100

    CategoryDao created

---
+++ src/main/java/ba/unsa/etf/rpr/dao/CategoryDao.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.dao;
+import ba.unsa.etf.rpr.domain.Category;
+
+/**
+ * Dao interface for Category domain bean
+ *
+ * @author Ernad Zuga
+ */
+public interface CategoryDao extends Dao<Category> {
+}
