43012f36be9aa4e8153a8de28f5eaca3a8d552c6
Ernad Zuga <ezuga1@etf.unsa.ba>
2022-12-19 12:56:04 +0100

    searchByPrice implemented in ItemsDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/ItemsDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ItemsDaoSQLImpl.java
@@ -104,6 +104,18 @@ public class ItemsDaoSQLImpl extends AbstractDao<Items> implements ItemsDao {
 
     @Override
     public List<Items> searchByPrice(String price) throws MarketException {
-        return null;
+        String query = "SELECT * FROM items WHERE price = ?";
+        try {
+            PreparedStatement stmt = getConnection().prepareStatement(query);
+            stmt.setString(1, price);
+            ResultSet rs = stmt.executeQuery();
+            ArrayList<Items> itemsLista = new ArrayList<>();
+            while(rs.next()){
+                itemsLista.add(row2object(rs));
+            }
+            return itemsLista;
+        }catch (SQLException e){
+            throw new MarketException(e.getMessage(),e);
+        }
     }
 }
