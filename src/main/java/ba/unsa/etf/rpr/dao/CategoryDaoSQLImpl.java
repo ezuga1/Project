1d19e77192f1b6150e9914a98d9cb9ebe1741bba
Ernad Zuga <ezuga1@etf.unsa.ba>
2022-12-25 16:25:37 +0100

    CategoryDaoSQLImpl corrected

--- src/main/java/ba/unsa/etf/rpr/dao/CategoryDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CategoryDaoSQLImpl.java
@@ -15,7 +15,7 @@ import java.util.TreeMap;
 
 public class CategoryDaoSQLImpl extends AbstractDao<Category> implements CategoryDao {
     public CategoryDaoSQLImpl() {
-        super("categories");
+        super("category");
     }
 
     @Override
