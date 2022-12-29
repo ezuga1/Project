b6ab24cbdca73977343bf6d51d0c9db47504d564
Ernad Zuga <ezuga1@etf.unsa.ba>
2022-12-29 15:44:57 +0100

    CategoryManager validateCategoryName created

--- src/main/java/ba/unsa/etf/rpr/business/CategoryManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/CategoryManager.java
@@ -1,4 +1,20 @@
 package ba.unsa.etf.rpr.business;
 
+
+import ba.unsa.etf.rpr.exceptions.MarketException;
+
+/**
+ * Business Logic Layer for management of Categories
+ *
+ * @author Ernad Zuga
+ */
+
 public class CategoryManager {
+    public void validateCategoryName(String name) throws MarketException {
+        if(name == null || name.length() > 45 || name.length() <3){
+            throw new MarketException("Category name must be between 3 and 45 chars");
+        }
+
+    }
+
 }
