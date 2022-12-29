887e315b9d390b50a2a72a20d9c325bce6491b33
Ernad Zuga <ezuga1@etf.unsa.ba>
2022-12-29 15:26:06 +0100

    AbstractDao executeQueryUnique added

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -169,7 +169,23 @@ public abstract class AbstractDao <T extends Idable> implements Dao<T>{
             throw new MarketException(e.getMessage(), e);
         }
     }
+    /**
+     * Utility for query execution that always return single record
+     * @param query - query that returns single record
+     * @param params - list of params for sql query
+     * @return Object
+     * @throws MarketException in case when object is not found
+     */
 
+    public T executeQueryUnique(String query, Object[] params) throws MarketException {
+        List<T> result  = executeQuery(query,params);
+        if(result != null && result.size() == 1){
+            return result.get(0);
+        }
+        else{
+            throw new MarketException("Object not found");
+        }
+    }
     /**
      * Accepts KV storage of column names and return CSV of columns and question marks for insert statement
      * Example: (id, name, date) ?,?,?
