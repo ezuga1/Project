3a369880fc73ef0662f229b0b85d2793d710f9fc
Ernad Zuga <ezuga1@etf.unsa.ba>
2022-12-29 14:36:17 +0100

    ItemsManager updated

--- src/main/java/ba/unsa/etf/rpr/business/ItemsManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ItemsManager.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.business;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Items;
+import ba.unsa.etf.rpr.exceptions.MarketException;
+
+import java.util.List;
+
+/**
+ * Business Logic Layer for Items
+ *
+ * @author Ernad Zuga
+ */
 
 public class ItemsManager {
 }
