ee184af12fe7d972b03c00784d7e73eb6796d277
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-01-06 23:41:14 +0100

    LoginController updated, not connected to db

--- src/main/java/ba/unsa/etf/rpr/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/LoginController.java
@@ -6,8 +6,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
-
+import javafx.stage.Window;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 import java.awt.*;
 import java.io.IOException;
 
@@ -16,9 +20,39 @@ public class LoginController {
     private Stage stage;
     private Scene scene;
 
+    @FXML
+    private TextField userNameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
 
     public void login(ActionEvent actionEvent) {
+        Window owner = loginButton.getScene().getWindow();
+        System.out.println(userNameField.getText());
+        System.out.println(passwordField.getText());
 
+        if(userNameField.getText().isEmpty()){
+            showAlert(Alert.AlertType.ERROR, owner, "Ooops, login error", "Please enter your username");
+            return;
+        }
+        if(passwordField.getText().isEmpty()){
+            showAlert(Alert.AlertType.ERROR, owner, "Ooops, login error", "Please enter your password");
+            return;
+        }
+
+
+    }
+
+    public static void showAlert(Alert.AlertType alertType, Window owner, String s, String message){
+        Alert alert = new Alert(alertType);
+        alert.setTitle(s);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
     }
     public void register(ActionEvent actionEvent) throws IOException {
         root = FXMLLoader.load(getClass().getResource("/fxml/register.fxml"));
