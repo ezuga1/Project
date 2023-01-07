ec52421ef5a8417ea9a00d836fa1ff619dd516a9
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-01-07 15:52:24 +0100

    AbstractDao, AbstractDao corrected

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -12,17 +12,8 @@ public abstract class AbstractDao <T extends Idable> implements Dao<T>{
     private String tableName;
 
     public AbstractDao(String tableName){
-        try {
-            this.tableName = tableName;
-            Properties p = new Properties();
-            p.load(ClassLoader.getSystemResource("app.properties").openStream());
-            String url = p.getProperty("db.connection_string");
-            String username = p.getProperty("db.username");
-            String password = p.getProperty("db.password");
-            this.connection = DriverManager.getConnection(url, username, password);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        this.tableName = tableName;
+        createConnection();
     }
     private static void createConnection(){
         if(AbstractDao.connection == null) {
@@ -52,7 +43,7 @@ public abstract class AbstractDao <T extends Idable> implements Dao<T>{
         }
     }
     public Connection getConnection(){
-        return this.connection;
+        return AbstractDao.connection;
     }
 
     public void setConnection(Connection connection) {
