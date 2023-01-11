4e9ba71b305852d40ef62b779e505eaec9b12607
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-01-11 10:48:39 +0100

    Home page of market created

--- src/main/java/ba/unsa/etf/rpr/MarketController.java
+++ src/main/java/ba/unsa/etf/rpr/MarketController.java
@@ -1,5 +1,27 @@
 package ba.unsa.etf.rpr;
 
+import javafx.event.ActionEvent;
+
 public class MarketController {
 
+    public void onClickHome(ActionEvent actionEvent) {
+    }
+
+    public void onClickShop(ActionEvent actionEvent) {
+    }
+
+    public void onClickContact(ActionEvent actionEvent) {
+    }
+
+    public void onClickLogin(ActionEvent actionEvent) {
+    }
+
+    public void onAddToCart1(ActionEvent actionEvent) {
+    }
+
+    public void onAddToCart2(ActionEvent actionEvent) {
+    }
+
+    public void onAddToCart3(ActionEvent actionEvent) {
+    }
 }
