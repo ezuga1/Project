8d2a432c1f1b2084d945e798ff4f2cf9664f66fa
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-01-14 21:52:28 +0100

    contact page finished and ContactController updated

--- src/main/java/ba/unsa/etf/rpr/ContactController.java
+++ src/main/java/ba/unsa/etf/rpr/ContactController.java
@@ -1,11 +1,32 @@
 package ba.unsa.etf.rpr;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class ContactController {
-    public void onClickLogOut(ActionEvent actionEvent) {
+    Parent root;
+    Stage stage;
+    Scene scene;
+    public void onClickLogOut(ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public void onClickHome(ActionEvent actionEvent) {
+    public void onClickHome(ActionEvent actionEvent) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("/fxml/market.fxml"));
+        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
