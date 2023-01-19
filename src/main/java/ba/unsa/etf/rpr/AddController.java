c4c398f6b75a5f4198da009fa8e556ee234acebf
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-01-19 22:19:28 +0100

    AdminController initialize() method created

---
+++ src/main/java/ba/unsa/etf/rpr/AddController.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr;
+
+import javafx.event.ActionEvent;
+
+public class AddController {
+    public void onAddItem(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/ba/unsa/etf/rpr/AdminController.java
+++ src/main/java/ba/unsa/etf/rpr/AdminController.java
@@ -36,6 +36,7 @@ public class AdminController {
 
     }
     public void addItem(ActionEvent actionEvent) {
+
     }
 
     public void deleteItem(ActionEvent actionEvent) {
