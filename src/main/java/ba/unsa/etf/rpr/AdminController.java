b993bd04b38d99aebb90563c5d05fc1cc88641bb
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-01-19 13:28:05 +0100

    admin.fxml created

---
+++ src/main/java/ba/unsa/etf/rpr/AdminController.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr;
+
+import javafx.event.ActionEvent;
+
+public class AdminController {
+    public void addItem(ActionEvent actionEvent) {
+    }
+
+    public void deleteItem(ActionEvent actionEvent) {
+    }
+
+    public void updateItem(ActionEvent actionEvent) {
+    }
+}
