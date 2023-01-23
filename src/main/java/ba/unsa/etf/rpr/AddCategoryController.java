c14930aca502a57698bd2e89f02d2e08366725f6
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-01-23 09:31:24 +0100

    AddCategoryController created

---
+++ src/main/java/ba/unsa/etf/rpr/AddCategoryController.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr;
+
+import ba.unsa.etf.rpr.business.ItemsManager;
+import ba.unsa.etf.rpr.domain.Items;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import java.awt.*;
+
+
+public class AddCategoryController {
+    @FXML
+    private TextField categoryName;
+
+    @FXML
+    private TextField categoryStatus;
+    public void onAddCategory(ActionEvent actionEvent) {
+
+    }
+}
--- src/main/java/ba/unsa/etf/rpr/AdminController.java
+++ src/main/java/ba/unsa/etf/rpr/AdminController.java
@@ -43,5 +43,6 @@ public class AdminController {
     }
 
     public void updateItem(ActionEvent actionEvent) {
+
     }
 }
