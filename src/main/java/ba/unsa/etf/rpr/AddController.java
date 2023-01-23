9a9a70273065f5b6a1d9a970c66ff8dc6f2cbebf
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-01-23 21:14:38 +0100

    AddController updated

--- src/main/java/ba/unsa/etf/rpr/AddController.java
+++ src/main/java/ba/unsa/etf/rpr/AddController.java
@@ -1,19 +1,47 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.business.ItemsManager;
+import ba.unsa.etf.rpr.domain.Items;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.MarketException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class AddController {
+    @FXML
+    private TextField txtItemName;
+    @FXML
+    private TextField txtItemPrice;
+    @FXML
+    private TextField ItemDescription;
+    @FXML
+    private TextField CategoryId;
     private Stage stage;
     private Scene scene;
     private Parent root;
-    public void onAddItem(ActionEvent actionEvent) {
+    public void onAddItem(ActionEvent actionEvent) throws MarketException {
+        ItemsManager itemsManager = new ItemsManager();
+        Items item = new Items();
+
+        String itemName = txtItemName.getText();
+        String itemPrice = txtItemPrice.getText();
+        String itemDescription = ItemDescription.getText();
+        int categoryId = Integer.parseInt(CategoryId.getText());
+
+        item.setItemName(itemName);
+        item.setPrice(itemPrice);
+        item.setDescription(itemDescription);
+        item.setCategoryID(categoryId);
+        item.setUserID(2);
+        itemsManager.add(item);
 
     }
 
