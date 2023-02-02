51e01cd02ceacc32f6a78ea7dc06bfd19a849903
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-02-03 00:46:38 +0100

    UserManager class finished

--- src/main/java/ba/unsa/etf/rpr/business/UserManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -3,9 +3,24 @@ import ba.unsa.etf.rpr.dao.Dao;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Category;
 import ba.unsa.etf.rpr.domain.Items;
+import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.MarketException;
 
 import java.util.List;
 
 public class UserManager {
+    public List<User> getAll() throws MarketException{
+        return DaoFactory.userDao().getAll();
+    }
+
+    public void delete (int id) throws MarketException{
+        DaoFactory.userDao().delete(id);
+    }
+    public User add(User user) throws MarketException{
+        return DaoFactory.userDao().add(user);
+
+    }
+    public void update(User user) throws MarketException{
+        DaoFactory.userDao().update(user);
+    }
 }
