9dd28f8dae4e61551a93bfa0e358fe32a10acaa4
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-02-05 13:24:05 +0100

    commons-cli dependency added

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,15 +1,10 @@
 package ba.unsa.etf.rpr;
 
-import ba.unsa.etf.rpr.business.ItemsManager;
-import ba.unsa.etf.rpr.domain.Items;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+
 
 /**
- * Hello world!
+ * Command Line Interface implementation in following class
+ * @author Ernad Zuga
  *
  */
 public class App{
