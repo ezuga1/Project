49ab200a6ba06ce5c89cfc3164d1af5c5c94e592
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-02-05 13:41:34 +0100

    Final variables defined in App

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -9,7 +9,8 @@ import ba.unsa.etf.rpr.domain.Category;
 import ba.unsa.etf.rpr.exceptions.MarketException;
 
 
-
+import net.bytebuddy.asm.Advice;
+import org.apache.commons.cli.*;
 
 
 
@@ -22,6 +23,10 @@ public class App{
     /**
      * Defining final variables to describe all code having options
      */
-
+    private static final Option addItem = new Option("i", "add-item",false, "Adding new item to Tech market database");
+    private static final Option addCategory = new Option("c","add-category",false,"Adding new category to Tech market database");
+    private static final Option getItems = new Option("getI", "get-items",false, "Printing all items from Tech market database");
+    private static final Option getCategories = new Option("getC","get-categories",false, "Printing all categories from Teck market database");
+    private static final Option categoryDefinition = new Option(null, "category", false, "Defining category for next added item");
 
 }
