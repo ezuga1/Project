26ea88811bb2c4d8c0c3f70fa9d096d69aeba99b
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-02-05 13:48:08 +0100

    addOptions method implemented

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -20,22 +20,36 @@ import java.io.PrintWriter;
  * @author Ernad Zuga
  *
  */
-public class App{
+public class App {
     /**
      * Defining final variables to describe all code having options
      */
-    private static final Option addItem = new Option("i", "add-item",false, "Adding new item to Tech market database");
-    private static final Option addCategory = new Option("c","add-category",false,"Adding new category to Tech market database");
-    private static final Option getItems = new Option("getI", "get-items",false, "Printing all items from Tech market database");
-    private static final Option getCategories = new Option("getC","get-categories",false, "Printing all categories from Teck market database");
+    private static final Option addItem = new Option("i", "add-item", false, "Adding new item to Tech market database");
+    private static final Option addCategory = new Option("c", "add-category", false, "Adding new category to Tech market database");
+    private static final Option getItems = new Option("getI", "get-items", false, "Printing all items from Tech market database");
+    private static final Option getCategories = new Option("getC", "get-categories", false, "Printing all categories from Teck market database");
     private static final Option categoryDefinition = new Option(null, "category", false, "Defining category for next added item");
 
 
-    public static void printFormattedOptions(Options options){
+    public static void printFormattedOptions(Options options) {
         HelpFormatter helpFormatter = new HelpFormatter();
         PrintWriter printWriter = new PrintWriter(System.out);
-        helpFormatter.printUsage(printWriter,150,"java -jar Project.jar [option] 'something else if needed' ");
-        helpFormatter.printOptions(printWriter,150, options, 2,7);
+        helpFormatter.printUsage(printWriter, 150, "java -jar Project.jar [option] 'something else if needed' ");
+        helpFormatter.printOptions(printWriter, 150, options, 2, 7);
         printWriter.close();
     }
+
+    public static Options addOptions() {
+        Options options = new Options();
+        options.addOption(addItem);
+        options.addOption(addCategory);
+        options.addOption(getItems);
+        options.addOption(getCategories);
+        options.addOption(categoryDefinition);
+        return options;
+    }
+
+
+
+
 }
