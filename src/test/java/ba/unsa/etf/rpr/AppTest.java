0b9e701a18119f76eeb8a2c2aafecf7cd5814232
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-02-07 23:13:51 +0100

    testAddCategory implemented in AppTest

--- src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -2,45 +2,63 @@ package ba.unsa.etf.rpr;
 
 import ba.unsa.etf.rpr.business.CategoryManager;
 import ba.unsa.etf.rpr.controllers.AddCategoryController;
+import ba.unsa.etf.rpr.domain.Category;
+import ba.unsa.etf.rpr.exceptions.MarketException;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
+import org.junit.Test;
+import org.testfx.api.FxRobot;
 import java.io.IOException;
+import org.testfx.framework.junit.ApplicationTest;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit tests for Project.
  * @author Ernad Zuga
  */
-public class AppTest 
-{   private AddCategoryController addCategoryController;
-    private CategoryManager categoryManager;
+public class AppTest extends ApplicationTest {
+   private Parent root;
+   private Stage stage;
+   private Scene scene;
+
+   private AddCategoryController addCategoryController;
 
     /**
-     * setting up page for adding category
+     * Method to set up page for adding category
      */
-    @BeforeEach
-    public void setUp() throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/addCategory.fxml"));
-        Parent root = loader.load();
-        addCategoryController = loader.getController();
-        Scene scene = new Scene(root);
-        Stage stage = new Stage();
-        stage.setScene(scene);
+
+   @Override
+   public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/category.fxml"));
+        stage.setScene(scene = new Scene(loader.load(),300,300));
         stage.show();
-        categoryManager = new CategoryManager();
-    }
+   }
+
     /**
-     * Test for add button
+     * Test for Add button
      */
     @Test
-    public void testAddButton(){
+    public void testAddCategory() throws MarketException {
+        FxRobot robot = new FxRobot();
+        robot.clickOn("#categoryName").write("Fruits");
+        robot.clickOn("#categoryStatus").write("Active");
+        robot.clickOn("#AddCategory");
 
+        Category c = new Category();
+        CategoryManager categoryManager = new CategoryManager();
+        for (Category tmp : categoryManager.getAll()) {
+            if (tmp.getName().equals("Fruits") && tmp.getStatus().equals("Active")) {
+                c = tmp;
+            }
+        }
+        assertNotNull(c);
+        assertEquals("Fruits", c.getName());
+        assertEquals("Active", c.getStatus());
     }
 }
