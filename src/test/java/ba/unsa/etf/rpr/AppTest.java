0b3cb740c7dd6ea653e8e9f0f77a86a9fc36aa95
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-02-07 22:20:27 +0100

    setUp method implemented in AppTest

--- src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -1,20 +1,46 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.business.CategoryManager;
+import ba.unsa.etf.rpr.controllers.AddCategoryController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- * Unit test for simple App.
+ * Unit tests for Project.
+ * @author Ernad Zuga
  */
 public class AppTest 
-{
+{   private AddCategoryController addCategoryController;
+    private CategoryManager categoryManager;
+
     /**
-     * Rigorous Test :-)
+     * setting up page for adding category
+     */
+    @BeforeEach
+    public void setUp() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/addCategory.fxml"));
+        Parent root = loader.load();
+        addCategoryController = loader.getController();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+        categoryManager = new CategoryManager();
+    }
+    /**
+     * Test for add button
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAddButton(){
+
     }
 }
