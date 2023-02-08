743465c0c793b7810249a8f84a4d672c460ad979
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-02-08 18:56:24 +0100

    Javadoc done for Exceptions package

--- src/main/java/ba/unsa/etf/rpr/exceptions/MarketException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/MarketException.java
@@ -1,5 +1,9 @@
 package ba.unsa.etf.rpr.exceptions;
 
+/**
+ * MarketException for Project usage, extends Exception class
+ * @author Ernad Zuga
+ */
 public class MarketException extends Exception{
     public MarketException(String msg, Exception reason){
         super(msg, reason);
