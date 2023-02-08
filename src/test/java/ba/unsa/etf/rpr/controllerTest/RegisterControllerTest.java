dd566481be85d356d03717a1554e0834ee510efb
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-02-08 14:03:18 +0100

    testLogin implemented in RegisterControllerTest

--- src/test/java/ba/unsa/etf/rpr/controllerTest/RegisterControllerTest.java
+++ src/test/java/ba/unsa/etf/rpr/controllerTest/RegisterControllerTest.java
@@ -1,17 +1,16 @@
 package ba.unsa.etf.rpr.controllerTest;
 
 import ba.unsa.etf.rpr.business.UserManager;
-import ba.unsa.etf.rpr.controllers.RegisterController;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.MarketException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import org.junit.Before;
 import org.junit.Test;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit.ApplicationTest;
-import javafx.application.Platform.*;
+
 import java.io.IOException;
 
 import static javafx.application.Platform.runLater;
@@ -27,12 +26,12 @@ public class RegisterControllerTest extends ApplicationTest {
     /**
      * Method to open registration page
      */
-    @Override
+   /* @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/register.fxml"));
        stage.setScene(scene = new Scene(loader.load(),300,300));
        stage.show();
-    }
+    }*/
 
     /**
      * Test for registration with correct input
@@ -75,6 +74,10 @@ public class RegisterControllerTest extends ApplicationTest {
             robot.clickOn("#userNameField").write("Test1");
             robot.clickOn("#passwordField").write("test1");
             robot.clickOn("#loginButton");
+
+           /*robot.lookup("#SearchPrice").tryQuery().isPresent();
+           Label lblPrice = robot.lookup("#SearchPrice").queryAs(Label.class);
+           assertNotNull(lblPrice);*/
 
         });
 
