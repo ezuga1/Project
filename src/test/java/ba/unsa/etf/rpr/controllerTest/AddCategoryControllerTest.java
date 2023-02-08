2d5bf843d50f97e3d6df26c335a94dc88f839b99
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-02-08 10:59:06 +0100

    AddCategoryControllerTest updated and AdminControllerTest class created

--- src/test/java/ba/unsa/etf/rpr/controllerTest/AddCategoryControllerTest.java
+++ src/test/java/ba/unsa/etf/rpr/controllerTest/AddCategoryControllerTest.java
@@ -1,28 +1,25 @@
-package ba.unsa.etf.rpr;
+package ba.unsa.etf.rpr.controllerTest;
 
 import ba.unsa.etf.rpr.business.CategoryManager;
 import ba.unsa.etf.rpr.controllers.AddCategoryController;
 import ba.unsa.etf.rpr.domain.Category;
 import ba.unsa.etf.rpr.exceptions.MarketException;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.Test;
 import org.testfx.api.FxRobot;
 import java.io.IOException;
 import org.testfx.framework.junit.ApplicationTest;
 
-import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit tests for Project.
+ * Unit test for AddCategoryController.
  * @author Ernad Zuga
  */
-public class AppTest extends ApplicationTest {
+public class AddCategoryControllerTest extends ApplicationTest {
    private Parent root;
    private Stage stage;
    private Scene scene;
