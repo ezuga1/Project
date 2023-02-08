22ef22504538facecbb568b88e1fca92f6fe545c
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-02-08 16:04:52 +0100

    Javadoc done for UserManager

--- src/main/java/ba/unsa/etf/rpr/business/UserManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -14,20 +14,48 @@ import java.util.List;
  * @author Ernad Zuga
  */
 public class UserManager {
+    /**
+     * Method for getting list of all users from database
+     * @return
+     * @throws MarketException
+     */
     public List<User> getAll() throws MarketException{
         return DaoFactory.userDao().getAll();
     }
 
+    /**
+     * Method for deleting user from database
+     * @param id
+     * @throws MarketException
+     */
     public void delete (int id) throws MarketException{
         DaoFactory.userDao().delete(id);
     }
+
+    /**
+     * Method for adding user to database
+     * @param user
+     * @return
+     * @throws MarketException
+     */
     public User add(User user) throws MarketException{
         return DaoFactory.userDao().add(user);
 
     }
+    /**
+     * Method for updating user in database
+     */
     public void update(User user) throws MarketException{
         DaoFactory.userDao().update(user);
     }
+
+    /**
+     * Method for validating user. Checks if user exists in database.
+     * @param name
+     * @param password
+     * @return
+     * @throws MarketException
+     */
     public boolean validate(String name, String password) throws MarketException {
         UserManager userManager = new UserManager();
         for(User u : userManager.getAll()){
