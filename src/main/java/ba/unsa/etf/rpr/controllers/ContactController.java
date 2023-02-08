33116655e6554869ed43d31c4cfca5b3425c18a8
Ernad Zuga <ezuga1@etf.unsa.ba>
2023-02-08 16:14:56 +0100

    Javadoc done for ContactController

--- src/main/java/ba/unsa/etf/rpr/controllers/ContactController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ContactController.java
@@ -9,11 +9,21 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Controller for Contact page
+ * @author Ernad Zuga
+ */
 
 public class ContactController {
     Parent root;
     Stage stage;
     Scene scene;
+
+    /**
+     * Method for Logout button, it returns us to login page
+     * @param actionEvent
+     * @throws IOException
+     */
     public void onClickLogOut(ActionEvent actionEvent) throws IOException {
         root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
         stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
@@ -22,6 +32,11 @@ public class ContactController {
         stage.show();
     }
 
+    /**
+     * Method for Home button, it returns us to home page
+     * @param actionEvent
+     * @throws IOException
+     */
     public void onClickHome(ActionEvent actionEvent) throws IOException{
         root = FXMLLoader.load(getClass().getResource("/fxml/market.fxml"));
         stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
